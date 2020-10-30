a9ae22bad7a54dd531a9506d0b985d0e9c9a89ae
pmorixe <morixe.pablo@gmail.com>
2020-10-30 16:20:30 -0300

    Create planning model tables init

---
+++ migrations/mysql/modelprod/20201030191514713_create_configuration_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `configuration` (
+  `logistic_center_id` varchar(10) NOT NULL,
+  `key` varchar(45) NOT NULL,
+  `value` int(11) NOT NULL,
+  `metri_unit` varchar(45) NOT NULL,
+  PRIMARY KEY (`logistic_center_id`,`key`)
+)

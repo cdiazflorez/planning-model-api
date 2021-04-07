4ccd3a5f1fc818110f6c59b2f4570db0b76fb836
Marcial Manzo <marcial.manzo@mercadolibre.com>
2021-04-07 12:10:21 -0300

    Adds remove forecasts to repository

--- src/main/java/com/mercadolibre/planning/model/api/client/db/repository/forecast/ForecastJpaRepository.java
+++ src/main/java/com/mercadolibre/planning/model/api/client/db/repository/forecast/ForecastJpaRepository.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.planning.model.api.client.db.repository.forecast;
 
+import com.mercadolibre.planning.model.api.domain.entity.Workflow;
 import com.mercadolibre.planning.model.api.domain.entity.forecast.Forecast;
 import com.mercadolibre.planning.model.api.domain.entity.forecast.ForecastMetadata;
 import com.mercadolibre.planning.model.api.gateway.ForecastGateway;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Component
@@ -27,5 +29,20 @@ public class ForecastJpaRepository implements ForecastGateway {
             entityManager.persist(metadata);
         });
         return forecast;
+    }
+
+    @Override
+    public int deleteOlderThan(final Workflow workflow, final ZonedDateTime date) {
+        final String queryStr =
+                "SELECT f FROM Forecast f WHERE f.lastUpdated < :date AND f.workflow = :workflow";
+
+        final List<Forecast> oldForecasts = entityManager.createQuery(queryStr, Forecast.class)
+                .setParameter("date", date)
+                .setParameter("workflow", workflow)
+                .getResultList();
+
+        oldForecasts.forEach(entityManager::remove);
+
+        return oldForecasts.size();
     }
 }
--- src/main/java/com/mercadolibre/planning/model/api/gateway/ForecastGateway.java
+++ src/main/java/com/mercadolibre/planning/model/api/gateway/ForecastGateway.java
@@ -1,11 +1,15 @@
 package com.mercadolibre.planning.model.api.gateway;
 
+import com.mercadolibre.planning.model.api.domain.entity.Workflow;
 import com.mercadolibre.planning.model.api.domain.entity.forecast.Forecast;
 import com.mercadolibre.planning.model.api.domain.entity.forecast.ForecastMetadata;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface ForecastGateway {
 
     Forecast create(final Forecast forecast, final List<ForecastMetadata> metadatas);
+
+    int deleteOlderThan(Workflow workflow, ZonedDateTime date);
 }

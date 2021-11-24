dc8edd0252c648a2abfe727ec43d4c4e221c684a
Marcial Manzo <marcial.manzo@mercadolibre.com>
2021-11-24 10:30:25 -0300

    return pay before with projections (#312)
    * extract GetCptProjectionUseCase class
    
    * uses extracted class
    
    * delivery promise projection output refactoring
    
    * adds pay before field to deliveery promise output

---
+++ src/main/java/com/mercadolibre/planning/model/api/domain/usecase/projection/capacity/input/GetCptProjectionInput.java
@@ -0,0 +1,31 @@
+package com.mercadolibre.planning.model.api.domain.usecase.projection.capacity.input;
+
+import com.mercadolibre.planning.model.api.domain.entity.ProcessName;
+import com.mercadolibre.planning.model.api.domain.entity.Workflow;
+import com.mercadolibre.planning.model.api.web.controller.projection.request.ProjectionType;
+import com.mercadolibre.planning.model.api.web.controller.projection.request.QuantityByDate;
+import lombok.Value;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Value
+public class GetCptProjectionInput {
+    private Workflow workflow;
+
+    private String warehouseId;
+
+    private ProjectionType type;
+
+    private List<ProcessName> processName;
+
+    private ZonedDateTime dateFrom;
+
+    private ZonedDateTime dateTo;
+
+    private List<QuantityByDate> backlog;
+
+    private String timeZone;
+
+    private boolean applyDeviation;
+}

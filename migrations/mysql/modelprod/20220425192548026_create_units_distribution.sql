66b7b46d0398218afbac67666a0a9f987191e84d
cdiazflorez <carlos.diazflorez@mercadolibre.com.co>
2022-04-25 15:17:10 -0500

    add index on units distribution

--- migrations/mysql/modelprod/20220425192548026_create_units_distribution.sql
+++ migrations/mysql/modelprod/20220425192548026_create_units_distribution.sql
@@ -1,12 +1,15 @@
 CREATE TABLE `units_distribution`
 (
     `id`                   bigint(8) NOT NULL AUTO_INCREMENT,
-    `area`                 varchar(45) NOT NULL,
     `date`                 datetime    NOT NULL,
     `logistic_center_id`   varchar(45) NOT NULL,
+    `workflow`             varchar(45) NOT NULL,
     `process_name`         varchar(45) NOT NULL,
+    `area`                 varchar(45) NOT NULL,
     `quantity`             bigint(8) NOT NULL,
     `quantity_metric_unit` varchar(45) NOT NULL,
-    `workflow`             varchar(45) NOT NULL,
     PRIMARY KEY (`id`)
 );
+
+create index idx_units_distribution_date_logistic_center_id
+    on units_distribution (date, logistic_center_id) using BTREE;

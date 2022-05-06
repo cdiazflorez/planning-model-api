7f8caada513feef8c9deb221f1bcb088a9363f1f
cdiazflorez <carlos.diazflorez@mercadolibre.com.co>
2022-05-06 14:06:39 -0500

    alter table

--- migrations/mysql/modelprod/20220506183546131_alter-units-distribution.sql
+++ migrations/mysql/modelprod/20220506183546131_alter-units-distribution.sql
@@ -1,3 +1,3 @@
 truncate units_distribution;
 
-ALTER table units_distribution MODIFY COLUMN quantity double(8,3) NOT NULL
+ALTER table units_distribution MODIFY COLUMN quantity double(8,3) NOT NULL;

9e7df913e226169f041837ef48ef552a10fc5452
Felipe Bautista <felipe.bautista@mercadolibre.com.co>
2022-07-29 13:00:37 -0500

    restore planning metadata repository test

--- src/test/java/com/mercadolibre/planning/model/api/client/db/repository/forecast/PlanningMetadataRepositoryTest.java
+++ src/test/java/com/mercadolibre/planning/model/api/client/db/repository/forecast/PlanningMetadataRepositoryTest.java
@@ -22,7 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class PlanningMetadataRepositoryTest {
 
     @Autowired

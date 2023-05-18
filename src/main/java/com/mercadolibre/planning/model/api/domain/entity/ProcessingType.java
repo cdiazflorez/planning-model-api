de3a4dfdf01483c6e4027ec77f4c6348ec1bbd66
Ivan Acosta Lallemand <ivan.acosta@mercadolibre.com.co>
2023-05-18 09:44:53 -0500

    Feature/add processing type for limits shipping 70 (#818)
    * add BACKLOG_LOWER_LIMIT_SHIPPING and BACKLOG_UPPER_LIMIT_SHIPPING
    
    * add new values
    
    * minutes to units
    
    * test
    
    * code quality
    
    * quality
    
    * quality
    
    * conflicts
    
    * test

--- src/main/java/com/mercadolibre/planning/model/api/domain/entity/ProcessingType.java
+++ src/main/java/com/mercadolibre/planning/model/api/domain/entity/ProcessingType.java
@@ -1,39 +1,38 @@
 package com.mercadolibre.planning.model.api.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import static java.util.stream.Collectors.toMap;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 
-import static java.util.stream.Collectors.toMap;
-
 public enum ProcessingType {
 
-    ACTIVE_WORKERS,
-    ACTIVE_WORKERS_NS,
-    EFFECTIVE_WORKERS,
-    EFFECTIVE_WORKERS_NS,
-    PERFORMED_PROCESSING,
-    REMAINING_PROCESSING,
-    MAX_CAPACITY,
-    BACKLOG_LOWER_LIMIT,
-    BACKLOG_UPPER_LIMIT,
-    BACKLOG_LOWER_LIMIT_SHIPPING,
-    BACKLOG_UPPER_LIMIT_SHIPPING,
-    THROUGHPUT;
+  ACTIVE_WORKERS,
+  ACTIVE_WORKERS_NS,
+  EFFECTIVE_WORKERS,
+  EFFECTIVE_WORKERS_NS,
+  PERFORMED_PROCESSING,
+  REMAINING_PROCESSING,
+  MAX_CAPACITY,
+  BACKLOG_LOWER_LIMIT,
+  BACKLOG_UPPER_LIMIT,
+  BACKLOG_LOWER_LIMIT_SHIPPING,
+  BACKLOG_UPPER_LIMIT_SHIPPING,
+  THROUGHPUT;
 
-    private static final Map<String, ProcessingType> LOOKUP = Arrays.stream(values()).collect(
-            toMap(ProcessingType::toString, Function.identity())
-    );
+  private static final Map<String, ProcessingType> LOOKUP = Arrays.stream(values()).collect(
+      toMap(ProcessingType::toString, Function.identity())
+  );
 
-    public static Optional<ProcessingType> of(final String value) {
-        return Optional.ofNullable(LOOKUP.get(value.toUpperCase()));
-    }
+  public static Optional<ProcessingType> of(final String value) {
+    return Optional.ofNullable(LOOKUP.get(value.toUpperCase()));
+  }
 
-    @JsonValue
-    public String toJson() {
-        return toString().toLowerCase();
-    }
+  @JsonValue
+  public String toJson() {
+    return toString().toLowerCase();
+  }
 }

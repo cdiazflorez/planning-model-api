f5ae5386756e9244235eb3d5aa8d6fac5ac5bb45
fpertuz <fabio.pertuz@mercadolibre.com.co>
2023-07-21 14:51:23 -0500

    FWPM-1785: :sparkles: :card_file_box: deferral data table migration PROD. Index for logistic center, date, updated

---
+++ migrations/mysql/modelprod/20230721195000270_outbound_deferral_data_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `outbound_deferral_data`
+(
+    `id`                 BIGINT(8) NOT NULL AUTO_INCREMENT,
+    `logistic_center_id` VARCHAR(6)  NOT NULL,
+    `date`               DATETIME    NOT NULL,
+    `cpt`                DATETIME    NOT NULL,
+    `status`             VARCHAR(15) NOT NULL,
+    `updated`            TINYINT(1) NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX                logistic_center_date_idx (`logistic_center_id`, `date`, `updated`)
+    );

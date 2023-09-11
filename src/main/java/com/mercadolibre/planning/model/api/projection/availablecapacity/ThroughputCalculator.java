ff37db58f39a002a10c347ec9653fb88b5c8acdb
Franco Joaquin Tagliero <franco.tagliero@mercadolibre.com>
2023-09-11 09:21:57 -0300

    Feature/#1883 util throughput calculator (#960)
    Este servicio se encarga de calcular la suma de valores contenidos del mapa, donde cada elemento del mapa está asociado a una hora de operación. Además, permite especificar un rango de tiempo dentro del cual se deben sumar los valores correspondientes a las horas contenidas en ese rango.
    
    El servicio funciona de la siguiente manera:
    
    Recibe un mapa llamado "tphByHour" donde las claves son horas exactas (por ejemplo, "10:00:00Z", "11:00:00Z") y los valores son números enteros que representan los tphs asociados a esas horas.
    
    También recibe dos instantes de tiempo: "from" y "to" que definen el rango de tiempo dentro del cual se deben considerar las horas del mapa.
    El servicio calcula la suma de los valores asociados a las horas contenidas en el rango especificado, teniendo en cuenta si es necesario calcular una fracción de hora.
    
    Por ejemplo, si el mapa contiene datos para las horas "10:00:00Z:1000", "11:00:00Z:1000", "12:00:00Z:1000", y el rango de tiempo es de "10:30:00Z" a "11:30:00Z", el servicio calculará la suma correspondiente a "10:30:00Z:500" y "11:30:00Z:500", resultando en una suma total de 1000.
    
    Este servicio se va a invocar una vez tengamos los projected end date de todos los cpts, por cada uno vamos a calcular el tph correspodiente desde el projected end date (from) y el cutOff (to) y de la lista de int que obtengamos nos quedamos con el minimo como capacidad disponible.
    
    Nota: Cambie la firma de la tarea porque me pareció mejor esta.
    Issue Jira, RFC

---
+++ src/main/java/com/mercadolibre/planning/model/api/projection/availablecapacity/ThroughputCalculator.java
@@ -0,0 +1,46 @@
+package com.mercadolibre.planning.model.api.projection.availablecapacity;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
+
+/**
+ * A service for calculating the sum of values within a specified time range based on a map of time-value pairs.
+ */
+final class ThroughputCalculator {
+
+  private static final double MINUTES_IN_A_HOUR = 60.0;
+
+  private ThroughputCalculator() {
+  }
+
+  /**
+   * Calculates the sum of values within the specified time range based on the provided time-value pairs.
+   *
+   * @param tphByHour A map of time-value pairs, where each entry represents a time
+   *                  and an associated integer value.
+   * @param from      The starting time of the desired range (inclusive).
+   * @param to        The ending time of the desired range (inclusive).
+   * @return The sum of values within the specified time range.
+   */
+  public static int totalWithinRange(final Map<Instant, Integer> tphByHour, final Instant from, final Instant to) {
+    return tphByHour.entrySet().stream()
+        .mapToInt(entry -> calculateValueInRange(entry.getValue(), calculateFractionOfHour(entry.getKey(), from, to)))
+        .sum();
+  }
+
+  private static Duration calculateFractionOfHour(final Instant hour, final Instant from, final Instant to) {
+    final Instant start = hour.isAfter(from) ? hour : from;
+    final Instant end = hour.plus(Duration.ofHours(1)).isBefore(to) ? hour.plus(Duration.ofHours(1)) : to;
+    return Duration.between(start, end);
+  }
+
+
+  private static int calculateValueInRange(final int value, final Duration fractionOfHour) {
+    if (fractionOfHour.toMinutes() <= 0) {
+      return 0;
+    }
+    final double proportion = fractionOfHour.toMinutes() / MINUTES_IN_A_HOUR;
+    return (int) (value * proportion);
+  }
+}

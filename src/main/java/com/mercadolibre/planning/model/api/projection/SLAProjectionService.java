1bcb82d180453e51ca84883e1f20c0f3e3c15d17
Franco Joaquin Tagliero <franco.tagliero@mercadolibre.com>
2023-09-14 09:36:02 -0300

    Feature/#1884 uc available capacity (#965)
    Agrega la clase AvailableCapacityUseCase con el método execute que reciba los inputs del controller y utiliza el [SLAProjectionService.execute](https://github.com/melisource/fury_planning-model-api/pull/953) pasandole los inputs y el projection type, utiliza el metodo [ThroughputCalculator.totalWithinRange](https://github.com/melisource/fury_planning-model-api/pull/960) de la clase ThroughputCalculator, una vez obtenido todos los tphs entre la hora del projected end date y el sla se queda con el mínimo, arma la respuesta con la capacidad minima que encontró y el resultado de la proyección. 
    
    **Nota**: Agregue un pequeño fix en el ThroughputCalculator que es cuando no existe el `projected end date` debe devolver 0, también agregué un test para este caso.

--- src/main/java/com/mercadolibre/planning/model/api/projection/SLAProjectionService.java
+++ src/main/java/com/mercadolibre/planning/model/api/projection/SLAProjectionService.java
@@ -9,7 +9,9 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
+import org.springframework.stereotype.Service;
 
+@Service
 public final class SLAProjectionService {
 
   private SLAProjectionService() {

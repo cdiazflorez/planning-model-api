38c083f093c2d23f47c97cf1ff4dc6bfe8c3aae5
alexyrc <alexy.rinconcapera@mercadolibre.com.co>
2023-10-18 13:05:32 -0500

    chage delete index sentence

--- migrations/mysql/modelprod/20231018160812091_add_current_processing_distribution_idx_lc_workflow_type_date.sql
+++ migrations/mysql/modelprod/20231018160812091_add_current_processing_distribution_idx_lc_workflow_type_date.sql
@@ -1,3 +1,3 @@
-DROP INDEX logistic_center_id ON current_processing_distribution;
+ALTER TABLE current_processing_distribution DROP INDEX logistic_center_id;
 CREATE INDEX idx_current_processing_distribution_lc_workflow_type_date
     USING BTREE ON current_processing_distribution (`logistic_center_id`, `workflow`, `type`, `date`) VISIBLE;

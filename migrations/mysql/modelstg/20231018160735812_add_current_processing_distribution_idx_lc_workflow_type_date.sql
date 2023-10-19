ceb431d1978ebef12433efa5699d752ac63038e2
alexyrc <alexy.rinconcapera@mercadolibre.com.co>
2023-10-19 10:08:42 -0500

    restore query

--- migrations/mysql/modelstg/20231018160735812_add_current_processing_distribution_idx_lc_workflow_type_date.sql
+++ migrations/mysql/modelstg/20231018160735812_add_current_processing_distribution_idx_lc_workflow_type_date.sql
@@ -1,10 +1,4 @@
-/*ALTER TABLE current_processing_distribution DROP INDEX logistic_center_id;
+ALTER TABLE current_processing_distribution DROP INDEX logistic_center_id;
 CREATE INDEX logistic_center_id
-    USING BTREE ON current_processing_distribution (`logistic_center_id`, `workflow`, `type`, `date`) VISIBLE;*/
+    USING BTREE ON current_processing_distribution (`logistic_center_id`, `workflow`, `type`, `date`) VISIBLE;
 
-ALTER TABLE current_processing_distribution
-    DROP
-        PRIMARY KEY;
-
-ALTER TABLE current_processing_distribution
-    ADD PRIMARY KEY (`logistic_center_id`, `workflow`, `type`, `date`);

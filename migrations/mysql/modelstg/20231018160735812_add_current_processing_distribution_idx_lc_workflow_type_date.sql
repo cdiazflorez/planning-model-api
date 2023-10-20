45c3f785c7c99ed3a6e260d7517e1547cbd8c968
alexyrc <alexy.rinconcapera@mercadolibre.com.co>
2023-10-20 09:13:30 -0500

    change name.

--- migrations/mysql/modelstg/20231018160735812_add_current_processing_distribution_idx_lc_workflow_type_date.sql
+++ migrations/mysql/modelstg/20231018160735812_add_current_processing_distribution_idx_lc_workflow_type_date.sql
@@ -1,4 +1,4 @@
-ALTER TABLE current_processing_distribution DROP INDEX logistic_center_id;
-CREATE INDEX logistic_center_id
+ALTER TABLE current_processing_distribution
+    DROP INDEX logistic_center_id;
+CREATE INDEX current_processing_distribution_idx_lc_workflow_type_date
     USING BTREE ON current_processing_distribution (`logistic_center_id`, `workflow`, `type`, `date`) VISIBLE;
-

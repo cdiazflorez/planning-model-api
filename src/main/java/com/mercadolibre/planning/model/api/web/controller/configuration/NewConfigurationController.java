f0bb9b73189cd4e7745425fd2a49ca4ba30d51d3
Felipe Bautista <felipe.bautista@mercadolibre.com.co>
2023-10-20 16:54:32 -0500

    ✨  get configurations

--- src/main/java/com/mercadolibre/planning/model/api/web/controller/configuration/NewConfigurationController.java
+++ src/main/java/com/mercadolibre/planning/model/api/web/controller/configuration/NewConfigurationController.java
@@ -5,12 +5,15 @@ import static java.util.stream.Collectors.toMap;
 import com.mercadolibre.planning.model.api.domain.entity.configuration.Configuration;
 import com.mercadolibre.planning.model.api.domain.usecase.configuration.ConfigurationUseCase;
 import com.newrelic.api.agent.Trace;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,16 +37,34 @@ public class NewConfigurationController {
       @RequestBody @Valid final Map<@NotBlank String, @NotBlank String> configurations) {
 
     return ResponseEntity.ok(
-        configurationUseCase.save(userId, logisticCenterId, configurations)
-            .stream()
-            .collect(
-                toMap(
-                    Configuration::getKey,
-                    Configuration::getValue,
-                    (o1, o2) -> o2
-                )
-            )
+        toResponse(configurationUseCase.save(userId, logisticCenterId, configurations))
     );
+  }
+
+  @Trace(dispatcher = true)
+  @GetMapping
+  public ResponseEntity<Map<String, String>> get(@PathVariable final String logisticCenterId,
+                                                 @RequestParam final Set<String> keys) {
+
+    final var configurations = configurationUseCase.get(logisticCenterId, keys);
+
+    if (configurations.isEmpty()) {
+      return ResponseEntity.noContent().build();
+    }
+
+    return ResponseEntity.ok(toResponse(configurations));
+
+  }
+
+  private Map<String, String> toResponse(final List<Configuration> configurations) {
+    return configurations.stream()
+        .collect(
+            toMap(
+                Configuration::getKey,
+                Configuration::getValue,
+                (o1, o2) -> o2
+            )
+        );
   }
 
 }
--- src/test/java/com/mercadolibre/planning/model/api/web/controller/configuration/NewConfigurationControllerTest.java
+++ src/test/java/com/mercadolibre/planning/model/api/web/controller/configuration/NewConfigurationControllerTest.java
@@ -4,7 +4,11 @@ import static com.mercadolibre.planning.model.api.util.TestUtils.LOGISTIC_CENTER
 import static com.mercadolibre.planning.model.api.util.TestUtils.USER_ID;
 import static com.mercadolibre.planning.model.api.util.TestUtils.getResourceAsString;
 import static java.lang.String.format;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,11 +17,14 @@ import com.mercadolibre.planning.model.api.domain.entity.configuration.Configura
 import com.mercadolibre.planning.model.api.domain.usecase.configuration.ConfigurationUseCase;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -34,6 +41,19 @@ class NewConfigurationControllerTest {
 
   private static final String VALUE_2 = "2.0";
 
+  private static final List<Configuration> CONFIGURATIONS = List.of(
+      Configuration.builder()
+          .logisticCenterId(LOGISTIC_CENTER_ID)
+          .key(KEY_1)
+          .value(VALUE_1)
+          .build(),
+      Configuration.builder()
+          .logisticCenterId(LOGISTIC_CENTER_ID)
+          .key(KEY_2)
+          .value(VALUE_2)
+          .build()
+  );
+
   @Autowired
   private MockMvc mvc;
 
@@ -47,26 +67,13 @@ class NewConfigurationControllerTest {
         KEY_1, VALUE_1,
         KEY_2, VALUE_2
     );
-    when(configurationUseCase.save(USER_ID, LOGISTIC_CENTER_ID, configurations)).thenReturn(
-        List.of(
-            Configuration.builder()
-                .logisticCenterId(LOGISTIC_CENTER_ID)
-                .key(KEY_1)
-                .value(VALUE_1)
-                .build(),
-            Configuration.builder()
-                .logisticCenterId(LOGISTIC_CENTER_ID)
-                .key(KEY_2)
-                .value(VALUE_2)
-                .build()
-        )
-    );
+    when(configurationUseCase.save(USER_ID, LOGISTIC_CENTER_ID, configurations)).thenReturn(CONFIGURATIONS);
     // WHEN
     final ResultActions result = mvc.perform(
         post(format(URL, LOGISTIC_CENTER_ID))
             .param("user_id", String.valueOf(USER_ID))
             .param("logistic_center_id", LOGISTIC_CENTER_ID)
-            .contentType(MediaType.APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
             .content(getResourceAsString("post_new_configuration_ok.json"))
     );
     // THEN
@@ -82,11 +89,39 @@ class NewConfigurationControllerTest {
         post(format(URL, LOGISTIC_CENTER_ID))
             .param("user_id", String.valueOf(USER_ID))
             .param("logistic_center_id", LOGISTIC_CENTER_ID)
-            .contentType(MediaType.APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
             .content(getResourceAsString("post_new_configuration_error.json"))
     );
     // THEN
     result.andExpect(status().isBadRequest());
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideGetConfiguration")
+  void testGetConfiguration(final List<Configuration> configurations) throws Exception {
+    // GIVEN
+    when(configurationUseCase.get(anyString(), anySet())).thenReturn(configurations);
+    // WHEN
+    final ResultActions result = mvc.perform(
+        get(URL)
+            .contentType(APPLICATION_JSON)
+            .param("logistic_center_id", LOGISTIC_CENTER_ID)
+            .param("keys", KEY_1, KEY_2)
+    );
+    // THEN
+    if (configurations.isEmpty()) {
+      result.andExpect(status().isNoContent());
+    } else {
+      result.andExpect(status().isOk())
+          .andExpect(content().json(getResourceAsString("new_configuration_response.json")));
+    }
+  }
+
+  private static Stream<Arguments> provideGetConfiguration() {
+    return Stream.of(
+        Arguments.of(CONFIGURATIONS),
+        Arguments.of(List.of())
+    );
   }
 
 }

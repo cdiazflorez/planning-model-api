64880e90a1159c57150fde7f3ecb7f0fe0fded05
Marcial Manzo <marcial.manzo@mercadolibre.com>
2023-10-24 14:18:48 -0300

    configure route-ets cache ttl to 15 minutes (#1070)

--- src/main/java/com/mercadolibre/planning/model/api/client/rest/config/CacheConfig.java
+++ src/main/java/com/mercadolibre/planning/model/api/client/rest/config/CacheConfig.java
@@ -1,7 +1,7 @@
 package com.mercadolibre.planning.model.api.client.rest.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
@@ -12,19 +12,19 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class CacheConfig {
 
-    private static final int TIME_DEFAULT_TO_EXPIRE = 60;
+  public static final String CPT_CACHE_NAME = "cpt_by_warehouse";
 
-    public static final String CPT_CACHE_NAME = "cpt_by_warehouse";
+  private static final Duration TTL = Duration.ofMinutes(15);
 
-    @Bean
-    public Caffeine caffeineConfig() {
-        return Caffeine.newBuilder().expireAfterWrite(TIME_DEFAULT_TO_EXPIRE, TimeUnit.MINUTES);
-    }
+  @Bean
+  public Caffeine caffeineConfig() {
+    return Caffeine.newBuilder().expireAfterWrite(TTL);
+  }
 
-    @Bean
-    public CacheManager cacheManager(Caffeine caffeine) {
-        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-        caffeineCacheManager.setCaffeine(caffeine);
-        return caffeineCacheManager;
-    }
+  @Bean
+  public CacheManager cacheManager(Caffeine caffeine) {
+    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+    caffeineCacheManager.setCaffeine(caffeine);
+    return caffeineCacheManager;
+  }
 }

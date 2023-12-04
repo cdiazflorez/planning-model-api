5b53b9546e2cfafa39093a40bca26e28d15c21c1
Franco Tagliero <franco.tagliero@mercadolibre.com>
2023-12-04 15:39:22 -0300

    the logic of the available capacity calculator is changed

--- src/main/java/com/mercadolibre/planning/model/api/projection/availablecapacity/AvailableCapacityUseCase.java
+++ src/main/java/com/mercadolibre/planning/model/api/projection/availablecapacity/AvailableCapacityUseCase.java
@@ -1,95 +1,133 @@
 package com.mercadolibre.planning.model.api.projection.availablecapacity;
 
-import static java.time.temporal.ChronoUnit.MINUTES;
-
 import com.mercadolibre.planning.model.api.domain.entity.ProcessName;
 import com.mercadolibre.planning.model.api.domain.entity.ProcessPath;
 import com.mercadolibre.planning.model.api.projection.SLAProjectionService;
 import com.mercadolibre.planning.model.api.projection.builder.PackingProjectionBuilder;
 import com.mercadolibre.planning.model.api.projection.builder.Projector;
 import com.mercadolibre.planning.model.api.projection.builder.SlaProjectionResult;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 @Service
 @AllArgsConstructor
 public class AvailableCapacityUseCase {
 
-  /**
-   * Executes the projection with the parameters received and then with the projection result calculates the throughput
-   * between the projected end date and the cutoff of the slas and responds with the minimum.
-   * @param executionDateFrom lower range of the time to be calculated
-   * @param executionDateTo upper range of the time to be calculated
-   * @param currentBacklog backlog that exists in each process opened by process path and sla
-   * @param forecastBacklog backlog that
-   * @param throughput processing power by operation hour
-   * @param cycleTimeBySla cycle time expressed in minutes for each sla
-   * @return If all sla could finish in the projection. it returns the lowest tph between all the projected end date and te cut off,
-   *     otherwise returns 0.
-   */
-
-  public AvailableCapacity execute(
-      final Instant executionDateFrom,
-      final Instant executionDateTo,
-      final Map<ProcessName, Map<ProcessPath, Map<Instant, Long>>> currentBacklog,
-      final Map<Instant, Map<ProcessPath, Map<Instant, Long>>> forecastBacklog,
-      final Map<ProcessName, Map<Instant, Integer>> throughput,
-      final Map<Instant, Integer> cycleTimeBySla
-  ) {
-    final Projector projector = new PackingProjectionBuilder();
-    final SlaProjectionResult projection = SLAProjectionService.execute(
-        executionDateFrom,
-        executionDateTo,
-        currentBacklog,
-        forecastBacklog,
-        throughput,
-        getCutOffs(cycleTimeBySla),
-        projector
-    );
-    final boolean slasWhereNotFinished = projection.slas().stream()
-        .map(SlaProjectionResult.Sla::projectedEndDate)
-        .anyMatch(Objects::isNull);
-
-    if (slasWhereNotFinished) {
-      return new AvailableCapacity(0, projection);
+    private static Map<Instant, Instant> getCutOffs(final Map<Instant, Integer> cycleTimeBySla) {
+        return cycleTimeBySla.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> calculateCutOffFromSla(entry.getKey(), entry.getValue())));
     }
 
-    final Map<Instant, Integer> minimumTphByHour = ThroughputCalculator.getMinimumTphValueByHour(throughput);
+    private static List<EndDateCutOffBySLA> getProjectedEndDateByCutOffsAndSLA(
+            final List<SlaProjectionResult.Sla> projectionResult,
+            final Map<Instant, Integer> cycleTimeBySla
+    ) {
+        return projectionResult.stream()
+                .map(projection -> new EndDateCutOffBySLA(
+                        projection.date(),
+                        projection.projectedEndDate(),
+                        calculateCutOffFromSla(projection.date(), cycleTimeBySla.getOrDefault(projection.date(), 0))
+                ))
+                .toList();
+    }
 
-    final var endDateCutOffTupleStream = getProjectedEndDateByCutOffs(projection.slas(), cycleTimeBySla);
-    final Integer capacity = endDateCutOffTupleStream
-        .map(tuple -> ThroughputCalculator.totalWithinRange(minimumTphByHour, tuple.projectedEndDate, tuple.cutOff))
-        .min(Comparator.naturalOrder())
-        .orElseThrow();
-    return new AvailableCapacity(capacity, projection);
-  }
+    private static Instant calculateCutOffFromSla(final Instant sla, final Integer cycleTime) {
+        return sla.minus(cycleTime, MINUTES);
+    }
 
-  private static Map<Instant, Instant> getCutOffs(final Map<Instant, Integer> cycleTimeBySla) {
-    return cycleTimeBySla.entrySet().stream()
-        .collect(Collectors.toMap(Map.Entry::getKey, entry -> calculateCutOffFromSla(entry.getKey(), entry.getValue())));
-  }
+    /**
+     * A sla that does not have capacity is a sla whose projected end date is null or is after it's cut off.
+     *
+     * @param endDateCutOffBySLA a stream of tuples that must be obtained the slas that do not have capacity.
+     * @return a list of {@link Instant} that represent those sla's with zero capacity.
+     */
+    private static List<Instant> getSlasWithoutCapacity(final List<EndDateCutOffBySLA> endDateCutOffBySLA) {
+        return endDateCutOffBySLA.stream()
+                .filter(record -> record.projectedEndDate() == null || record.projectedEndDate().isAfter(record.cutOff()))
+                .map(EndDateCutOffBySLA::date)
+                .toList();
+    }
 
-  private static Stream<EndDateCutOffTuple> getProjectedEndDateByCutOffs(
-      final List<SlaProjectionResult.Sla> projectionResult,
-      final Map<Instant, Integer> cycleTimeBySla
-  ) {
-    return projectionResult.stream()
-        .map(projection -> new EndDateCutOffTuple(
-            projection.projectedEndDate(),
-            calculateCutOffFromSla(projection.date(), cycleTimeBySla.getOrDefault(projection.date(), 0))
-        ));
-  }
+    private static List<CapacityBySLA> calculateMinCapacityBySLA(final List<CapacityBySLA> capacityBySLA) {
+        return capacityBySLA.stream()
+                .map(currentHour -> new CapacityBySLA(currentHour.date(), getMinCapBetweenSlaAndFollowingOnes(capacityBySLA, currentHour)))
+                .toList();
+    }
 
-  private static Instant calculateCutOffFromSla(final Instant sla, final Integer cycleTime) {
-    return sla.minus(cycleTime, MINUTES);
-  }
+    /**
+     * Obtains the minimum capacity between the Sla received and the following Sla.
+     *
+     * @param capacityBySLA SLA list with its available capacity.
+     * @param sinceSLA      SLA in which the available slow shipment capacity is calculated.
+     * @return a {@link CapacityBySLA} that has the SLA and its available capacity taking into account the following SLAs
+     */
+    private static int getMinCapBetweenSlaAndFollowingOnes(final List<CapacityBySLA> capacityBySLA, final CapacityBySLA sinceSLA) {
+        return capacityBySLA.stream()
+                .filter(sla -> !sla.date().isBefore(sinceSLA.date()))
+                .mapToInt(CapacityBySLA::capacity)
+                .min()
+                .orElse(sinceSLA.capacity());
+    }
 
-  record EndDateCutOffTuple(Instant projectedEndDate, Instant cutOff) {}
+    /**
+     * Executes the projection with the parameters received and then with the projection result calculates the throughput
+     * between the projected end date and the cutoff of the slas and responds with the minimum capacity by each SLA.s
+     *
+     * @param executionDateFrom lower range of the time to be calculated
+     * @param executionDateTo   upper range of the time to be calculated
+     * @param currentBacklog    backlog that exists in each process opened by process path and sla
+     * @param forecastBacklog   backlog that
+     * @param throughput        processing power by operation hour
+     * @param cycleTimeBySla    cycle time expressed in minutes for each sla
+     * @return the minimum capacity calculated by each SLA.
+     */
+    public List<CapacityBySLA> execute(
+            final Instant executionDateFrom,
+            final Instant executionDateTo,
+            final Map<ProcessName, Map<ProcessPath, Map<Instant, Long>>> currentBacklog,
+            final Map<Instant, Map<ProcessPath, Map<Instant, Long>>> forecastBacklog,
+            final Map<ProcessName, Map<Instant, Integer>> throughput,
+            final Map<Instant, Integer> cycleTimeBySla
+    ) {
+        final Projector projector = new PackingProjectionBuilder();
+        final SlaProjectionResult projection = SLAProjectionService.execute(
+                executionDateFrom,
+                executionDateTo,
+                currentBacklog,
+                forecastBacklog,
+                throughput,
+                getCutOffs(cycleTimeBySla),
+                projector
+        );
+
+        final var endDateCutOffBySLA = getProjectedEndDateByCutOffsAndSLA(projection.slas(), cycleTimeBySla);
+
+        final var slasWithoutCapacity = getSlasWithoutCapacity(endDateCutOffBySLA);
+        final var capacityBySLAWithoutCapacity = slasWithoutCapacity.stream()
+                .map(each -> new CapacityBySLA(each, 0));
+
+        final var capacityBySLAWithCapacity = endDateCutOffBySLA.stream()
+                .filter(each -> !slasWithoutCapacity.contains(each.date));
+
+        final Map<Instant, Integer> minimumTphByHour = ThroughputCalculator.getMinimumTphValueByHour(throughput);
+        final var capacityBySLAS = capacityBySLAWithCapacity
+                .map(tuple -> new CapacityBySLA(tuple.date, ThroughputCalculator.totalWithinRange(minimumTphByHour, tuple.projectedEndDate, tuple.cutOff)));
+        final var capacityBySLA = Stream.concat(capacityBySLAS, capacityBySLAWithoutCapacity)
+                .sorted(Comparator.comparing(CapacityBySLA::date))
+                .toList();
+
+        return calculateMinCapacityBySLA(capacityBySLA);
+    }
+
+    record EndDateCutOffBySLA(Instant date, Instant projectedEndDate, Instant cutOff) {
+    }
 }
--- src/test/java/com/mercadolibre/planning/model/api/projection/AvailableCapacityUseCaseTest.java
+++ src/test/java/com/mercadolibre/planning/model/api/projection/AvailableCapacityUseCaseTest.java
@@ -1,28 +1,11 @@
 package com.mercadolibre.planning.model.api.projection;
 
-import static com.mercadolibre.planning.model.api.domain.entity.ProcessName.BATCH_SORTER;
-import static com.mercadolibre.planning.model.api.domain.entity.ProcessName.PACKING;
-import static com.mercadolibre.planning.model.api.domain.entity.ProcessName.PACKING_WALL;
-import static com.mercadolibre.planning.model.api.domain.entity.ProcessName.PICKING;
-import static com.mercadolibre.planning.model.api.domain.entity.ProcessName.WALL_IN;
-import static com.mercadolibre.planning.model.api.domain.entity.ProcessName.WAVING;
-import static com.mercadolibre.planning.model.api.domain.entity.ProcessPath.TOT_MONO;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
-
 import com.mercadolibre.planning.model.api.domain.entity.ProcessName;
 import com.mercadolibre.planning.model.api.domain.entity.ProcessPath;
 import com.mercadolibre.planning.model.api.projection.availablecapacity.AvailableCapacityUseCase;
+import com.mercadolibre.planning.model.api.projection.availablecapacity.CapacityBySLA;
 import com.mercadolibre.planning.model.api.projection.builder.Projector;
 import com.mercadolibre.planning.model.api.projection.builder.SlaProjectionResult;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -33,111 +16,149 @@ import org.mockito.InjectMocks;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static com.mercadolibre.planning.model.api.domain.entity.ProcessName.*;
+import static com.mercadolibre.planning.model.api.domain.entity.ProcessPath.TOT_MONO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class AvailableCapacityUseCaseTest {
 
-  private static final Instant DATE_1 = Instant.parse("2023-09-12T10:00:00Z");
-  private static final Instant DATE_2 = Instant.parse("2023-09-12T13:00:00Z");
-  private static final Instant SLA_1 = Instant.parse("2023-09-12T13:00:00Z");
-  private static final Instant SLA_2 = Instant.parse("2023-09-12T14:00:00Z");
-  private static final Instant SLA_3 = Instant.parse("2023-09-12T15:00:00Z");
-  private static final Instant SLA_4 = Instant.parse("2023-09-12T16:00:00Z");
-  private static final Instant END_DATE_1 = Instant.parse("2023-09-12T12:00:00Z");
-  private static final Instant END_DATE_2 = Instant.parse("2023-09-12T13:00:00Z");
-  private static final Instant END_DATE_3 = Instant.parse("2023-09-12T14:00:00Z");
-  private static final Instant END_DATE_4 = Instant.parse("2023-09-12T15:00:00Z");
-  private static final double ZERO = 0D;
-  private static final Map<ProcessName, Map<ProcessPath, Map<Instant, Long>>> CURRENT_BACKLOG = Map.of(
-      PICKING, Map.of(TOT_MONO, Map.of(SLA_1, 1L))
-  );
-  private static final Map<Instant, Map<ProcessPath, Map<Instant, Long>>> FORECAST_BACKLOG = Map.of(
-      DATE_1, Map.of(TOT_MONO, Map.of(SLA_1, 0L)));
-  private static final Map<ProcessName, Map<Instant, Integer>> THROUGHPUT = Map.of(
-      WAVING, Map.of(DATE_1, 500, END_DATE_1, 3000, END_DATE_2, 400, END_DATE_3, 200, END_DATE_4, 100),
-      PICKING, Map.of(DATE_1, 1000, END_DATE_1, 1000, END_DATE_2, 400, END_DATE_3, 200, END_DATE_4, 80),
-      PACKING, Map.of(DATE_1, 1500, END_DATE_1, 200, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100),
-      PACKING_WALL, Map.of(DATE_1, 500, END_DATE_1, 200, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100),
-      BATCH_SORTER, Map.of(DATE_1, 50, END_DATE_1, 2000, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100),
-      WALL_IN, Map.of(DATE_1, 100, END_DATE_1, 2000, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100)
-  );
-  private static final Map<Instant, Integer> CYCLE_TIME_BY_SLA = Map.of(SLA_1, 30, SLA_2, 30, SLA_3, 30, SLA_4, 30);
-  private static final Map<Instant, Instant> CUT_OFF_BY_SLA = Map.of(
-      SLA_1, SLA_1.minus(30, ChronoUnit.MINUTES),
-      SLA_2, SLA_2.minus(30, ChronoUnit.MINUTES),
-      SLA_3, SLA_3.minus(30, ChronoUnit.MINUTES),
-      SLA_4, SLA_4.minus(30, ChronoUnit.MINUTES)
-  );
-  private static MockedStatic<SLAProjectionService> mockedSettings;
+    private static final Instant DATE_1 = Instant.parse("2023-09-12T10:00:00Z");
+    private static final Instant DATE_2 = Instant.parse("2023-09-12T13:00:00Z");
+    private static final Instant SLA_1 = Instant.parse("2023-09-12T13:00:00Z");
+    private static final Instant SLA_2 = Instant.parse("2023-09-12T14:00:00Z");
+    private static final Instant SLA_3 = Instant.parse("2023-09-12T15:00:00Z");
+    private static final Instant SLA_4 = Instant.parse("2023-09-12T16:00:00Z");
+    private static final Instant END_DATE_1 = Instant.parse("2023-09-12T12:00:00Z");
+    private static final Instant END_DATE_2 = Instant.parse("2023-09-12T13:00:00Z");
+    private static final Instant END_DATE_3 = Instant.parse("2023-09-12T14:00:00Z");
+    private static final Instant END_DATE_4 = Instant.parse("2023-09-12T15:00:00Z");
+    private static final double ZERO = 0D;
+    private static final Map<ProcessName, Map<ProcessPath, Map<Instant, Long>>> CURRENT_BACKLOG = Map.of(
+            PICKING, Map.of(TOT_MONO, Map.of(SLA_1, 1L))
+    );
+    private static final Map<Instant, Map<ProcessPath, Map<Instant, Long>>> FORECAST_BACKLOG = Map.of(
+            DATE_1, Map.of(TOT_MONO, Map.of(SLA_1, 0L)));
+    private static final Map<ProcessName, Map<Instant, Integer>> THROUGHPUT = Map.of(
+            WAVING, Map.of(DATE_1, 500, END_DATE_1, 3000, END_DATE_2, 400, END_DATE_3, 200, END_DATE_4, 100),
+            PICKING, Map.of(DATE_1, 1000, END_DATE_1, 1000, END_DATE_2, 400, END_DATE_3, 200, END_DATE_4, 80),
+            PACKING, Map.of(DATE_1, 1500, END_DATE_1, 200, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100),
+            PACKING_WALL, Map.of(DATE_1, 500, END_DATE_1, 200, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100),
+            BATCH_SORTER, Map.of(DATE_1, 50, END_DATE_1, 2000, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100),
+            WALL_IN, Map.of(DATE_1, 100, END_DATE_1, 2000, END_DATE_2, 200, END_DATE_3, 200, END_DATE_4, 100)
+    );
+    private static final Map<Instant, Integer> CYCLE_TIME_BY_SLA = Map.of(SLA_1, 30, SLA_2, 30, SLA_3, 30, SLA_4, 30);
+    private static final Map<Instant, Instant> CUT_OFF_BY_SLA = Map.of(
+            SLA_1, SLA_1.minus(30, ChronoUnit.MINUTES),
+            SLA_2, SLA_2.minus(30, ChronoUnit.MINUTES),
+            SLA_3, SLA_3.minus(30, ChronoUnit.MINUTES),
+            SLA_4, SLA_4.minus(30, ChronoUnit.MINUTES)
+    );
+    private static MockedStatic<SLAProjectionService> mockedSettings;
 
-  @InjectMocks
-  private AvailableCapacityUseCase availableCapacityUseCase;
+    @InjectMocks
+    private AvailableCapacityUseCase availableCapacityUseCase;
 
 
-  @BeforeAll
-  public static void init() {
-    mockedSettings = mockStatic(SLAProjectionService.class);
-  }
+    @BeforeAll
+    public static void init() {
+        mockedSettings = mockStatic(SLAProjectionService.class);
+    }
 
-  @AfterAll
-  public static void close() {
-    mockedSettings.close();
-  }
+    @AfterAll
+    public static void close() {
+        mockedSettings.close();
+    }
 
-  public static Stream<Arguments> testCases() {
-    return Stream.of(
-          Arguments.of(
-              new SlaProjectionResult(List.of(new SlaProjectionResult.Sla(SLA_1, END_DATE_1, ZERO))),
-              200
-          ),
-        Arguments.of(
-            new SlaProjectionResult(List.of(
-                new SlaProjectionResult.Sla(SLA_1, END_DATE_1, ZERO),
-                new SlaProjectionResult.Sla(SLA_2, END_DATE_2, ZERO),
-                new SlaProjectionResult.Sla(SLA_3, END_DATE_3, ZERO),
-                new SlaProjectionResult.Sla(SLA_4, END_DATE_4, ZERO)
-            )),
-            40
-        ),
-        Arguments.of(
-            new SlaProjectionResult(List.of(
-                new SlaProjectionResult.Sla(SLA_1, END_DATE_1, ZERO),
-                new SlaProjectionResult.Sla(SLA_2, END_DATE_2, ZERO),
-                new SlaProjectionResult.Sla(SLA_3, null, 1D),
-                new SlaProjectionResult.Sla(SLA_4, END_DATE_4, ZERO)
-            )),
-            0
-        )
-      );
-  }
+    public static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of(
+                        new SlaProjectionResult(List.of(new SlaProjectionResult.Sla(SLA_1, END_DATE_1, ZERO))),
+                        List.of(new CapacityBySLA(SLA_1, 200))
+                ),
+                Arguments.of(
+                        new SlaProjectionResult(List.of(
+                                new SlaProjectionResult.Sla(SLA_1, END_DATE_1, ZERO),
+                                new SlaProjectionResult.Sla(SLA_2, END_DATE_2, ZERO),
+                                new SlaProjectionResult.Sla(SLA_3, END_DATE_3, ZERO),
+                                new SlaProjectionResult.Sla(SLA_4, END_DATE_4, ZERO)
+                        )),
+                        List.of(new CapacityBySLA(SLA_1, 40), new CapacityBySLA(SLA_2, 40), new CapacityBySLA(SLA_3, 40), new CapacityBySLA(SLA_4, 40))
+                ),
+                Arguments.of(
+                        new SlaProjectionResult(List.of(
+                                new SlaProjectionResult.Sla(SLA_1, END_DATE_1, ZERO),
+                                new SlaProjectionResult.Sla(SLA_2, END_DATE_2, ZERO),
+                                new SlaProjectionResult.Sla(SLA_3, null, 1D),
+                                new SlaProjectionResult.Sla(SLA_4, END_DATE_4, ZERO)
+                        )),
+                        List.of(new CapacityBySLA(SLA_1, 0), new CapacityBySLA(SLA_2, 0), new CapacityBySLA(SLA_3, 0), new CapacityBySLA(SLA_4, 40))
+                ),
+                Arguments.of(
+                        new SlaProjectionResult(List.of(
+                                new SlaProjectionResult.Sla(SLA_1, null, 55),
+                                new SlaProjectionResult.Sla(SLA_2, END_DATE_2, ZERO),
+                                new SlaProjectionResult.Sla(SLA_3, END_DATE_3, ZERO),
+                                new SlaProjectionResult.Sla(SLA_4, END_DATE_4, ZERO)
+                        )),
+                        List.of(
+                                new CapacityBySLA(SLA_1, 0),
+                                new CapacityBySLA(SLA_2, 40),
+                                new CapacityBySLA(SLA_3, 40),
+                                new CapacityBySLA(SLA_4, 40))
+                ),
+                Arguments.of(
+                        new SlaProjectionResult(List.of(
+                                new SlaProjectionResult.Sla(SLA_1, END_DATE_1, ZERO),
+                                new SlaProjectionResult.Sla(SLA_2, null, 500),
+                                new SlaProjectionResult.Sla(SLA_3, END_DATE_3, ZERO),
+                                new SlaProjectionResult.Sla(SLA_4, END_DATE_4, ZERO)
+                        )),
+                        List.of(
+                                new CapacityBySLA(SLA_1, 0),
+                                new CapacityBySLA(SLA_2, 0),
+                                new CapacityBySLA(SLA_3, 40),
+                                new CapacityBySLA(SLA_4, 40))
+                )
+        );
+    }
 
-  @ParameterizedTest
-  @MethodSource("testCases")
-  void testAvailableCapacity(
-      final SlaProjectionResult mockProjectionService,
-      final int expectedCapacity
-      ) {
-    when(SLAProjectionService.execute(
-        eq(DATE_1),
-        eq(DATE_2),
-        eq(CURRENT_BACKLOG),
-        eq(FORECAST_BACKLOG),
-        eq(THROUGHPUT),
-        eq(CUT_OFF_BY_SLA),
-        any(Projector.class)
-    )).thenReturn(mockProjectionService);
+    @ParameterizedTest
+    @MethodSource("testCases")
+    void testAvailableCapacity(
+            final SlaProjectionResult mockProjectionService,
+            final List<CapacityBySLA> expectedCapacity
+    ) {
+        when(SLAProjectionService.execute(
+                eq(DATE_1),
+                eq(DATE_2),
+                eq(CURRENT_BACKLOG),
+                eq(FORECAST_BACKLOG),
+                eq(THROUGHPUT),
+                eq(CUT_OFF_BY_SLA),
+                any(Projector.class)
+        )).thenReturn(mockProjectionService);
 
-    final var obtainedCapacity = availableCapacityUseCase.execute(
-        DATE_1,
-        DATE_2,
-        CURRENT_BACKLOG,
-        FORECAST_BACKLOG,
-        THROUGHPUT,
-        CYCLE_TIME_BY_SLA
+        final var obtainedCapacity = availableCapacityUseCase.execute(
+                DATE_1,
+                DATE_2,
+                CURRENT_BACKLOG,
+                FORECAST_BACKLOG,
+                THROUGHPUT,
+                CYCLE_TIME_BY_SLA
         );
 
-    // THEN
-    assertEquals(expectedCapacity, obtainedCapacity.capacity());
-    assertEquals(mockProjectionService, obtainedCapacity.projection());
-
-  }
+        // THEN
+        assertEquals(expectedCapacity, obtainedCapacity);
+    }
 }

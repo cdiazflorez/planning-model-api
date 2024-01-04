a0ff7ade2c7b935a8678eec7d58440f191cb5174
Edwin Orlando Gonzalez Yate <edwin.gonzalez@mercadolibre.com.co>
2024-01-04 12:53:02 -0500

    Add calculated and remaining for OutboundBuilder class :ok_hand: (#1112)
    Add methods for build calculate projected end date and remaining quantity in oubtound builder projection using utils the same methods for fromwavingBuilder and PackingBuilder

--- src/main/java/com/mercadolibre/planning/model/api/projection/builder/OutboundProjectionBuilder.java
+++ src/main/java/com/mercadolibre/planning/model/api/projection/builder/OutboundProjectionBuilder.java
@@ -15,6 +15,7 @@ import static com.mercadolibre.planning.model.api.projection.builder.ProjectorUt
 import static com.mercadolibre.planning.model.api.projection.builder.ProjectorUtils.buildOrderedBacklogByProcessPathProcessContexts;
 import static com.mercadolibre.planning.model.api.projection.builder.ProjectorUtils.buildPiecewiseUpstream;
 import static com.mercadolibre.planning.model.api.projection.builder.ProjectorUtils.buildSimpleProcess;
+import static com.mercadolibre.planning.model.api.projection.builder.ProjectorUtils.calculateRemainingQuantity;
 import static java.util.Collections.emptyMap;
 
 import com.mercadolibre.flow.projection.tools.services.entities.context.ContextsHolder;
@@ -30,14 +31,19 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-
+/**
+ * This class is used to project the necessary processes for outbound operation.
+ */
 public class OutboundProjectionBuilder implements Projector {
 
   static final String OUTBOUND_PROCESS_GROUP = "outbound_group";
 
-  static final Set<ProcessName> PROCESS_NAMES = Set.of(PACKING, BATCH_SORTER, WALL_IN, PACKING_WALL, HU_ASSEMBLY, SALES_DISPATCH);
+  static final Set<ProcessName> POST_PICKING_PROCESS = Set.of(PACKING, BATCH_SORTER, WALL_IN, PACKING_WALL, HU_ASSEMBLY, SALES_DISPATCH);
 
   static final Set<ProcessName> PRE_PICKING_PROCESS = Set.of(WAVING, PICKING);
+
+  static final Set<ProcessName> OUTBOUND_PROCESSES =
+      Set.of(WAVING, PICKING, PACKING, BATCH_SORTER, WALL_IN, PACKING_WALL, HU_ASSEMBLY, SALES_DISPATCH);
 
 
   /**
@@ -55,6 +61,13 @@ public class OutboundProjectionBuilder implements Projector {
         .build();
   }
 
+  /**
+   * Constructs context holder for the outbound operations.
+   *
+   * @param backlog    Backlogs which includes timestamp and path of each process.
+   * @param throughput Maximum output for each processes at different timestamps.
+   * @return Ordered and organized holder of contexts based on input parameters.
+   */
   @Override
   public ContextsHolder buildContextHolder(final Map<ProcessName, Map<ProcessPath, Map<Instant, Long>>> backlog,
                                            final Map<ProcessName, Map<Instant, Integer>> throughput) {
@@ -68,7 +81,8 @@ public class OutboundProjectionBuilder implements Projector {
         .filter(entry -> !PRE_PICKING_PROCESS.contains(entry.getKey()))
         .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-    final var postPickingContext = buildOrderedBacklogByDateBasedProcessesContexts(postPickingBacklog, throughput, PROCESS_NAMES);
+    final var postPickingContext =
+        buildOrderedBacklogByDateBasedProcessesContexts(postPickingBacklog, throughput, POST_PICKING_PROCESS);
 
     return buildBaseContextHolder(postPickingContext)
         .oneProcessContext(WAVING.getName(), wavingContext)
@@ -76,18 +90,47 @@ public class OutboundProjectionBuilder implements Projector {
         .build();
   }
 
+  /**
+   * Transforms forecasted backlogs into a PiecewiseUpstream object.
+   *
+   * @param forecast The map of forecasted backlogs.
+   * @return PiecewiseUpstream instance representing upstream model based on forecasted backlogs.
+   */
   @Override
   public PiecewiseUpstream toUpstream(final Map<Instant, Map<ProcessPath, Map<Instant, Long>>> forecast) {
     return buildPiecewiseUpstream(forecast);
   }
 
+  /**
+   * Calculates projected end dates based on the SLAs and provided contexts holder.
+   *
+   * @param slas   List of SLAs whose projected end dates are to be calculated.
+   * @param holder Contexts holder based on which projected end dates are to be calculated.
+   * @return SlaProjectionResult instance encapsulating the projected end dates.
+   */
   @Override
-  public SlaProjectionResult calculateProjectedEndDate(List<Instant> slas, ContextsHolder holder) {
-    return new SlaProjectionResult(List.of());
+  public SlaProjectionResult calculateProjectedEndDate(final List<Instant> slas, final ContextsHolder holder) {
+    return ProjectorUtils.calculateProjectedEndDate(
+        slas,
+        holder,
+        OUTBOUND_PROCESSES,
+        OUTBOUND_PROCESS_GROUP);
   }
 
+  /**
+   * Computes the remaining production quantity that has not been accomplished until the SLAs cut off.
+   *
+   * @param updatedContext Contexts holder object updated after operations.
+   * @param cutOff         Cut-off time for the SLAs.
+   * @return Map of SLA instances to the corresponding remaining production quantities.
+   */
   @Override
-  public Map<Instant, Long> getRemainingQuantity(ContextsHolder updatedContext, Map<Instant, Instant> cutOff) {
-    return Map.of();
+  public Map<Instant, Long> getRemainingQuantity(final ContextsHolder updatedContext, final Map<Instant, Instant> cutOff) {
+
+    final List<String> processes = updatedContext.getProcessContextByProcessName().keySet().stream()
+        .filter(s -> !s.contains(OUTBOUND_PROCESS_GROUP))
+        .toList();
+
+    return calculateRemainingQuantity(updatedContext, processes, cutOff);
   }
 }
